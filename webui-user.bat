78ce030cf2ace216d5278a4cda2238e224f33ad2
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2024-01-06 01:24:18 +0800

    modified:   webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -4,7 +4,7 @@ set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
 set PYTHON=
 set GIT=
 set VENV_DIR=./venv
-set COMMANDLINE_ARGS="--skip-python-version-check"
+@REM set COMMANDLINE_ARGS="--skip-python-version-check"
 
 
 call webui.bat
