dc2fad4f3867d685fab80a4d1887c9f3132e014a
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2024-01-06 01:15:41 +0800

    modified:   .gitignore 	modified:   webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
 
+set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
 set PYTHON=
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=./venv
+set COMMANDLINE_ARGS="--skip-python-version-check"
+
 
 call webui.bat
